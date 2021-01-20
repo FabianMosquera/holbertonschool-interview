fd4528f92889e00de474adb4006beda4cf696a8f
FabianMosquera <rfmq9530@gmail.com>
2021-01-20 17:34:36 -0500

    [FIX] Library

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,32 +1,31 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * is_palindrome - Entry Point
- * @head: Head of the list
- * Return: 1 if it is palindrome
+ * is_palindrome_recursion - function that checks if a singly is a palindrome
+ * @h: double pointer to head of list
+ * @tmp: pointer to travel the list recursively
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+int is_palindrome_recursion(listint_t **h, listint_t *tmp)
+{
+	int res;
+
+	if (tmp == NULL)
+		return (1);
+	res = is_palindrome_recursion(h, tmp->next);
+	if (tmp->n != (*h)->n)
+		return (0);
+	(*h) = (*h)->next;
+	return (res);
+}
+/**
+ * is_palindrome - function that checks if a singly linked list is a palindrome
+ * @head: pointer to head of list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *tmp = *head;
-	int array[4352], i = 0, j = 0, mid;
+	listint_t *h = *head;
 
-	if (head == NULL || (tmp != NULL && tmp->next == NULL))
-		return (1);
-
-	while (tmp != NULL)
-	{
-		array[i] = tmp->n;
-		tmp = tmp->next;
-		i++;
-	}
-	i--, mid = i / 2;
-	while (i >= mid && j <= mid)
-	{
-		if (array[i] != array[j])
-			return (0);
-		i--, j++;
-	}
-	return (1);
+	return (is_palindrome_recursion(&h, *head));
 }
